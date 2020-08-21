7fd45de9ecb3cd8eb7e872f09f9f94035b626e25
Anton <anton@Antons-MBP.Dlink>
2020-08-21 06:21:45 +0200

    Server running

--- main.dart
+++ main.dart
@@ -4,4 +4,10 @@ import 'dart:io';
 Future main() async {
   HttpServer server = await HttpServer.bind('127.0.0.1', 8080);
   print('Listening on Localhost:${server.port}');
+
+  server.listen((HttpRequest req) {
+    req.response
+      ..write('AWE')
+      ..close();
+  });
 }
