b97ee6d5dbf14ddc135427ada68484208dd279c7
Anton <anton@Antons-MBP.Dlink>
2020-08-21 06:47:56 +0200

    Catch Errors

--- main.dart
+++ main.dart
@@ -1,13 +1,34 @@
 import 'dart:async';
 import 'dart:io';
 
+final File file = File('index.html');
+
 Future main() async {
-  HttpServer server = await HttpServer.bind('127.0.0.1', 8080);
+  HttpServer server;
+  try {
+    server = await HttpServer.bind('127.0.0.1', 8080);
+  } on Exception catch (e) {
+    print('Failed to start server $e');
+    exit(-1);
+  }
   print('Listening on Localhost:${server.port}');
 
-  server.listen((HttpRequest req) {
-    req.response
-      ..write('AWE')
-      ..close();
-  });
+  await for (var req in server) {
+    if (await file.exists()) {
+      print('Serving ${file.path}');
+      req.response.headers.contentType = ContentType.html;
+
+      try {
+        await file.openRead().pipe(req.response);
+      } on Exception catch (e) {
+        print('Couldn\'t read file $e');
+        exit(-1);
+      }
+    } else {
+      print('Can\'t open file');
+      req.response
+        ..statusCode = HttpStatus.notFound
+        ..close();
+    }
+  }
 }
